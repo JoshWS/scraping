66551d0ff681ecc3d00af16f0e73db1846a5138c
JoshWS <JoshXteen@gmail.com>
2022-08-16 17:55:16 +0700

    all date inputed

--- site_scraping.py
+++ site_scraping.py
@@ -7,62 +7,58 @@ page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
 conn = sqlite3.connect('data.db')
-
 db = conn.cursor()
 
 db.execute("""CREATE TABLE IF NOT EXISTS articles (
             title TEXT,
-            links TEXT,
-            body TEXT,
+            link TEXT,
+            summary TEXT,
             dates TEXT
             )""")
 
 conn.commit()
 
-articles = {}
 
-news_title = []
+titles = []
 links = []
-body = []
+summarys = []
 dates = []
 
 
-
-
 def data_entry():
-    titles = doc.find_all(class_='news-title')
-    for b in titles:
-        title = b.find('a').string
-        news_title.append(title)
-        
 
-    article_locations = doc.find_all(class_='news-image')
-    for a in article_locations:
+    link_locations = doc.find_all(class_='news-image')
+    for a in link_locations:
         urls = a.find('a')
         full_urls = f"https://www.myanmar-now.org{urls['href']}"
         links.append(full_urls)
 
+
+    title_locations = doc.find_all(class_='news-title')
+    for b in title_locations:
+        title = b.find('a').string
+        titles.append(title)
     
+
+    summary_locations = doc.find_all(class_='news-body')
+    for c in summary_locations:
+        summary = c.string
+        summarys.append(summary)
+
+
+    date_locations = doc.find_all(class_='news-date')
+    for d in date_locations:
+        date = d.string 
+        dates.append(date)
 
 
 data_entry()
 
 
-
 for i in range(len(links)):
-    articles[news_title[i]] = links[i]
-    #db.execute("""INSERT INTO articles (title, links) VALUES (?, ?)""", (news_title[i], links[i]))
+    db.execute("""INSERT INTO articles (title, link, summary, dates) VALUES (?, ?, ?, ?)""", (titles[i], links[i], summarys[i], dates[i]))
     conn.commit()
 
-
-body_locations = doc.find_all(class_='news-body')
-
-
-
-print(body_locations)
-
-
 conn.commit()
-
 
 conn.close()
