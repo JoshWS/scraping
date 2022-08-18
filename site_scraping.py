6a1c04903f577ab9002297517d293f79e1914a61
JoshWS <JoshXteen@gmail.com>
2022-08-18 17:43:53 +0700

    yes

--- site_scraping.py
+++ site_scraping.py
@@ -2,8 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 import sqlite3
 
-pages = int(input("How many pages would you like to scrape? " + "\n"))
+# ask for user input for amount of pages
 
+
+while True:
+    try:
+        pages = int(input("How many pages would you like to scrape and display? " + "\n"))
+    except ValueError:
+        print('Sorry Please try a number form 1 - 100')
+        continue
+    else:
+        if pages <= 0:
+            continue
+        else:
+            if pages > 100:
+                continue
+            else:
+                break
+
+# iterate through each page and grabs all the information from the articles that I scrape for 
 for x in range(pages):
 
     url = f"https://www.myanmar-now.org/en/news?page={x}"
@@ -14,6 +31,7 @@ for x in range(pages):
     db = conn.cursor()
 
     #makes table if not exists
+    #db.execute("""DROP TABLE articles""")
     db.execute("""CREATE TABLE IF NOT EXISTS articles (
                 article_ID  INTEGER PRIMARY KEY,
                 title TEXT,
@@ -31,7 +49,7 @@ for x in range(pages):
     summarys = []
     dates = []
 
-
+    # finds the locations of the each piece of data and isolates the data a want in the format I need it in
     def data_entry():
 
         link_locations = doc.find_all(class_='news-image')
@@ -71,5 +89,28 @@ for x in range(pages):
 
 
     conn.commit()
-
     conn.close()
+
+
+conn = sqlite3.connect('data.db')
+db = conn.cursor()
+
+# displays the data in a organized way
+displayed_data = pages * 13
+iterations = 0
+db.execute("""SELECT * FROM articles""")
+data = db.fetchall()
+for d in data:
+    iterations += 1
+    if iterations > displayed_data:
+        break
+    
+    print(f"----------{d[0]}----------")
+    print(f"TITLE: {d[1]}")
+    print(f"LINK: {d[2]}")
+    print(f"SUMMARY: {d[3]}")
+    print(f"DATE: {d[4]}")
+    print('\n')
+
+conn.commit()
+conn.close()
